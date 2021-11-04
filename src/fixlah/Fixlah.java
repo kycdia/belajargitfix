23925d238875b32a5c7bbc84225e02d039059eb0
dickyadi <dickyadi44@gmail.com>
2021-11-05 00:27:10 +0700

    commit baru lagi

--- src/fixlah/Fixlah.java
+++ src/fixlah/Fixlah.java
@@ -16,6 +16,7 @@ public class Fixlah {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("apalah ini terserah");
     }
     
 }
